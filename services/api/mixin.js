5ef081e51c8753b9b1f4868af6828e5cf79d4c4a
NodeWee <nodewee@gmail.com>
2022-09-30 00:51:26 +0800

    update mixin api

--- services/api/mixin.js
+++ services/api/mixin.js
@@ -1,64 +1,25 @@
+'use strict'
+
 import http from '../../services/http/mixin'
-import storageUtil from '../../utils/storageUtil'
 
-export async function getAccessToken(code) {
-  // const verifier = localStorage.getItem('code-verifier')
-  const data = {
-    client_id: process.env.MIXIN_CLIENT_ID,
-    code,
-    client_secret: process.env.MIXIN_SECRET_KEY,
-    // code_verifier: verifier,
-  }
-  const res = await http.post('/oauth/token', { data })
-  if (res?.access_token) {
-    return res.access_token
-  }
-}
-
-export function getUser(id) {
-  return http.get(`/users/${id}`)
-}
-
-export function getProfile() {
-  return http.get('/me')
-}
-
-export function loginFunc(token) {
-  storageUtil.set('mixin_token', token)
-}
-
-export function getMixinContext() {
-  let ctx = {}
-  if (
-    window.webkit &&
-    window.webkit.messageHandlers &&
-    window.webkit.messageHandlers.MixinContext
-  ) {
-    ctx = JSON.parse(prompt('MixinContext.getContext()'))
-    ctx.platform = ctx.platform || 'iOS'
-  } else if (
-    window.MixinContext &&
-    typeof window.MixinContext.getContext === 'function'
-  ) {
-    ctx = JSON.parse(window.MixinContext.getContext())
-    ctx.platform = ctx.platform || 'Android'
-  }
-  return ctx
-}
-
-export function reloadTheme(platform) {
-  switch (platform) {
-    case 'iOS':
-      window.webkit &&
-        window.webkit.messageHandlers &&
-        window.webkit.messageHandlers.reloadTheme &&
-        window.webkit.messageHandlers.reloadTheme.postMessage('')
-      return
-    case 'Android':
-    case 'Desktop':
-      window.MixinContext &&
-        typeof window.MixinContext.reloadTheme === 'function' &&
-        window.MixinContext.reloadTheme()
-      return
-  }
+export const mixinApi = {
+  getAccessToken: async function getAccessToken(code) {
+    // const verifier = localStorage.getItem('code-verifier')
+    const data = {
+      client_id: process.env.MIXIN_CLIENT_ID,
+      code: code,
+      client_secret: process.env.MIXIN_SECRET_KEY,
+      // code_verifier: verifier,
+    }
+    const rsp = await http.post('/oauth/token', { data })
+    if (rsp?.access_token) {
+      return rsp.access_token
+    }
+  },
+  getUser: function (id) {
+    return http.get(`/users/${id}`)
+  },
+  getProfile: function () {
+    return http.get('/me')
+  },
 }
---
+++ utils/pageUtil.js
@@ -0,0 +1,37 @@
+export function getMixinContext() {
+  let ctx = {}
+  if (
+    window.webkit &&
+    window.webkit.messageHandlers &&
+    window.webkit.messageHandlers.MixinContext
+  ) {
+    ctx = JSON.parse(prompt('MixinContext.getContext()'))
+    ctx.platform = ctx.platform || 'iOS'
+  } else if (
+    window.MixinContext &&
+    typeof window.MixinContext.getContext === 'function'
+  ) {
+    ctx = JSON.parse(window.MixinContext.getContext())
+    ctx.platform = ctx.platform || 'Android'
+  }
+
+  return ctx
+}
+
+export function reloadTheme(platform) {
+  switch (platform) {
+    case 'iOS':
+      window.webkit &&
+        window.webkit.messageHandlers &&
+        window.webkit.messageHandlers.reloadTheme &&
+        window.webkit.messageHandlers.reloadTheme.postMessage('')
+      return
+    case 'Android':
+    // case 'Desktop':
+    default:
+      window.MixinContext &&
+        typeof window.MixinContext.reloadTheme === 'function' &&
+        window.MixinContext.reloadTheme()
+      return
+  }
+}
