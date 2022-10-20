e726067c0e86c3d3cf7c4474058e347a8486736b
NodeWee <nodewee@gmail.com>
2022-10-20 18:11:07 +0800

    update User page

--- components/User/index.js
+++ components/User/index.js
@@ -4,38 +4,48 @@ import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
 import toast from 'react-hot-toast'
-
+import { RiLogoutBoxLine } from 'react-icons/ri'
 import { TbMailbox } from 'react-icons/tb'
+import { Button } from '@nextui-org/react'
 
+import { logout } from '../../utils/loginUtil'
 import { getMixinContext } from '../../utils/pageUtil'
-import { getUserWallets } from '../../services/api/owl'
-import { handelOwlApiError } from '../../utils/apiUtils'
+import { getUserWallets } from '../../services/api/infowoods'
+import { handleInfowoodsApiError } from '../../utils/apiUtils'
 import { CurrentLoginContext } from '../../contexts/currentLogin'
-const SubPageCard = dynamic(() => import('../../widgets/SubPageCard'))
-const OwlToast = dynamic(() => import('../../widgets/OwlToast'))
+import SubPageCard from '../../widgets/SubPageCard'
+import Loading from '../../widgets/Loading'
+
+const Toast = dynamic(() => import('../../widgets/Toast'))
 const TopUpSheet = dynamic(() => import('./TopUpSheet'))
 const Wallets = dynamic(() => import('./Wallets'))
 
 import styles from './index.module.scss'
-import { logout } from '../../utils/loginUtil'
 
 function User() {
   const { t } = useTranslation('common')
   const [curLogin, _] = useContext(CurrentLoginContext)
-  const [inProcessOfTopUp, setInProcessOfTopUp] = useState(false)
+  const [ctx, setCtx] = useState({})
+  const [showTopupSheet, setShowTopupSheet] = useState(false)
+  const [logoutIng, setLogoutIng] = useState(false)
   const router = useRouter()
+
+  useEffect(() => {
+    const ctx = getMixinContext()
+    setCtx(ctx)
+  }, [])
 
   function useMyWallets() {
     const { data, error, mutate } = useSWR('me?wallets', getUserWallets)
     if (error) {
-      handelOwlApiError(error, t, curLogin)
+      handleInfowoodsApiError(error, t, curLogin)
     }
     return {
       data: data,
       isLoading: !error && !data,
       isError: error,
       refresh: () => {
-        mutate('me?wallets')
+        mutate()
       },
     }
   }
@@ -48,47 +58,56 @@ function User() {
         t={t}
         toast={toast}
         myWallets={myWallets}
-        setInProcessOfTopUp={setInProcessOfTopUp}
+        showTopupSheet={showTopupSheet}
+        setShowTopupSheet={setShowTopupSheet}
       ></Wallets>
 
-      <SubPageCard
-        LeftIcon={TbMailbox}
-        title={t('my-subscriptions')}
-        path={'/user/subscriptions'}
-      ></SubPageCard>
+      <div className={styles.cards}>
+        <SubPageCard
+          LeftIcon={TbMailbox}
+          title={t('my-subscriptions')}
+          path={'/user/subscriptions'}
+        ></SubPageCard>
 
-      <SubPageCard
-        LeftIcon={TbMailbox}
-        title={t('old-ver-subs')}
-        path={'/user/old-ver-subs'}
-      ></SubPageCard>
+        <SubPageCard
+          LeftIcon={TbMailbox}
+          title={t('old-ver-subs')}
+          path={'/user/old-ver-subs'}
+        ></SubPageCard>
+      </div>
 
       <div className={styles.logout}>
-        <span
-          onClick={() => {
+        <Button
+          icon={logoutIng ? <Loading size="sm" /> : <RiLogoutBoxLine />}
+          color="success"
+          size="md"
+          flat
+          onPress={() => {
+            setLogoutIng(true)
             const ctx = getMixinContext()
             logout(ctx.conversation_id)
             window.location.href = '/'
           }}
         >
           {t('logout')}
-        </span>
+        </Button>
       </div>
 
       {/* 充值组件 */}
-      {inProcessOfTopUp && (
-        <TopUpSheet
-          t={t}
-          toast={toast}
-          myWallets={myWallets}
-          handelOwlApiErrorP={(error) => {
-            handelOwlApiError(error, t, curLogin)
-          }}
-          setInProcessOfTopUp={setInProcessOfTopUp}
-        />
-      )}
 
-      <OwlToast />
+      <TopUpSheet
+        ctx={ctx}
+        t={t}
+        curLogin={curLogin}
+        myWallets={myWallets}
+        handelOwlApiErrorP={(error) => {
+          handleInfowoodsApiError(error, t, curLogin)
+        }}
+        showTopupSheet={showTopupSheet}
+        setShowTopupSheet={setShowTopupSheet}
+      />
+
+      <Toast />
     </div>
   )
 }
